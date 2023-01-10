f670aac59d0c5dc1e29c540893e81b4af3e63f82
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2023-01-11 00:06:54 +0100

    first commit

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,16 @@
+from battery.battery import Battery
+from datetime import date
+class NubbinBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+        
+        
+    def needs_service(self):
+        battery_serviced_date = date(self.last_service_date)
+        if battery_serviced_date < self.current_date:
+            return True
+        else:
+            return False
+        
+        
---
+++ car.py
@@ -0,0 +1,19 @@
+from servicing import Servicing
+
+class Car(Servicing):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+        
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
+
+
+
+
+        
+        
+    
+    
+    
+        
